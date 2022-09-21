179f76389878527cfa1b7c41072f4f4ec13488d6
EliGolam <elistasibusiness@gmail.com>
2022-09-21 14:42:00 +0200

    Added Bonus

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="title">
+        <h1>My First App with Vue CLI!</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppTitle',
+    }
+</script>
+
+<style scoped>
+    h1 {
+        color: white;
+    }
+</style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="hero-bg">
+        <div class="img-container">
+            <img src="../assets/img/01.jpg" alt="">
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HeroBG',
+    }
+</script>
+
+<style scoped lang="scss">
+    .img-container {
+        position: absolute;
+            top: 0;
+            z-index: -1;
+
+        object-fit: cover;
+    }
+</style>
